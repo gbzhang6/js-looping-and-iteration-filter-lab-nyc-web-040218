652cf94a384f1a415563906a4e027e6a9020f858
github username <github email address>
2018-05-16 17:30:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+  const newArr = []
+
+  for (const driver of collection) {
+    if (driver.toUpperCase() === name.toUpperCase())
+    {
+      newArr.push(driver)
+    }
+  }
+  return newArr
+}
+
+function fuzzyMatch(collection, name) {
+  const newArr = []
+
+  for (const driver of collection) {
+    if (driver.startsWith(name)) {
+      newArr.push(driver)
+    }
+  }
+  return newArr
+}
+
+function matchName(collection, name) {
+  const newArr = []
+
+  for(const driver of collection) {
+    if (driver.name === name) {
+      newArr.push(driver)
+    }
+  }
+  return newArr
+}
